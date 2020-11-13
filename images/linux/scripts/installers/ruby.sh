de82cd382565ba90d2036f23cddd96ef9f712e76
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-11-13 14:42:52 +0300

    pre-cache ruby binaries

--- images/linux/scripts/installers/ruby.sh
+++ images/linux/scripts/installers/ruby.sh
@@ -4,8 +4,43 @@
 ##  Desc:  Installs Ruby requirements
 ################################################################################
 
+source $HELPER_SCRIPTS/install.sh
+
 sudo apt-get install ruby-full
 sudo gem update --system
 
 # Install Ruby requirements
 apt-get install -y libz-dev openssl libssl-dev
+
+echo "Install Ruby..."
+toolset="$INSTALLER_SCRIPT_FOLDER/toolset.json"
+RELEASE_URL="https://api.github.com/repos/ruby/ruby-builder/releases/latest"
+VERSIONS=$(jq -r '.ruby.versions[]' $toolset)
+PLATFORM_VERSION=$(jq -r '.ruby.platform_version' $toolset)
+RUBY_PATH="$AGENT_TOOLSDIRECTORY/Ruby"
+
+echo "Check if Ruby hostedtoolcache folder exist..."
+if [ ! -d $RUBY_PATH ]; then
+    mkdir -p $RUBY_PATH
+fi
+
+for VERSION in ${VERSIONS[@]}; do
+    PACKAGE_TAR_NAME=$(curl $RELEASE_URL | jq -r '.assets[].name' | grep "^ruby-${VERSION}-ubuntu-${PLATFORM_VERSION}.tar.gz$" | sort -V | tail -1)
+    RUBY_VERSION=$(echo $PACKAGE_TAR_NAME | cut -d'-' -f 2)
+    PACKAGE_TAR_TEMP_PATH="/tmp/$PACKAGE_TAR_NAME"
+    RUBY_VERSION_PATH="$RUBY_PATH/$RUBY_VERSION"
+    mkdir -p $RUBY_VERSION_PATH
+
+    echo "Downloading tar archive $PACKAGE_TAR_NAME"
+    DOWNLOAD_URL="https://github.com/ruby/ruby-builder/releases/download/toolcache/${PACKAGE_TAR_NAME}"
+    download_with_retries $DOWNLOAD_URL $PACKAGE_TAR_TEMP_PATH
+
+    echo "Expand '$PACKAGE_TAR_NAME' to the '$RUBY_VERSION_PATH' folder"
+    tar xf $PACKAGE_TAR_TEMP_PATH -C $RUBY_VERSION_PATH
+
+    COMPLETE_FILE_PATH="$RUBY_VERSION_PATH/x64.complete"
+    if [ ! -f $COMPLETE_FILE_PATH ]; then
+        echo "Create complete file"    
+        touch $COMPLETE_FILE_PATH
+    fi
+done
