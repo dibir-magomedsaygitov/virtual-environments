531053d109e4f6be1090e0aa88236afa59038042
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-11-16 20:19:15 +0300

    resolve issues

--- images/linux/scripts/installers/ruby.sh
+++ images/linux/scripts/installers/ruby.sh
@@ -14,7 +14,7 @@ apt-get install -y libz-dev openssl libssl-dev
 
 echo "Install Ruby..."
 toolset="$INSTALLER_SCRIPT_FOLDER/toolset.json"
-RELEASE_URL="https://api.github.com/repos/ruby/ruby-builder/releases/latest"
+PACKAGE_TAR_NAMES=$(curl -s "https://api.github.com/repos/ruby/ruby-builder/releases/latest" | jq -r '.assets[].name')
 TOOLSET_VERSIONS=$(jq -r '.toolcache[] | select(.name | contains("ruby")) | .versions[]' $toolset)
 PLATFORM_VERSION=$(jq -r '.toolcache[] | select(.name | contains("ruby")) | .platform_version' $toolset)
 RUBY_PATH="$AGENT_TOOLSDIRECTORY/Ruby"
@@ -25,18 +25,19 @@ if [ ! -d $RUBY_PATH ]; then
 fi
 
 for TOOLSET_VERSION in ${TOOLSET_VERSIONS[@]}; do
-    PACKAGE_TAR_NAME=$(curl $RELEASE_URL | jq -r '.assets[].name' | grep "^ruby-${TOOLSET_VERSION}-ubuntu-${PLATFORM_VERSION}.tar.gz$" | sort -V | tail -1)
+    PACKAGE_TAR_NAME=$(echo $PACKAGE_TAR_NAMES | grep "^ruby-${TOOLSET_VERSION}-ubuntu-${PLATFORM_VERSION}.tar.gz$" | sort -V | tail -1)
     RUBY_VERSION=$(echo $PACKAGE_TAR_NAME | cut -d'-' -f 2)
-    PACKAGE_TAR_TEMP_PATH="/tmp/$PACKAGE_TAR_NAME"
     RUBY_VERSION_PATH="$RUBY_PATH/$RUBY_VERSION"
+    
+    echo "Create Ruby $RUBY_VERSION directory..."
     mkdir -p $RUBY_VERSION_PATH
 
     echo "Downloading tar archive $PACKAGE_TAR_NAME"
     DOWNLOAD_URL="https://github.com/ruby/ruby-builder/releases/download/toolcache/${PACKAGE_TAR_NAME}"
-    download_with_retries $DOWNLOAD_URL $PACKAGE_TAR_TEMP_PATH
+    download_with_retries $DOWNLOAD_URL "/tmp" $PACKAGE_TAR_NAME
 
     echo "Expand '$PACKAGE_TAR_NAME' to the '$RUBY_VERSION_PATH' folder"
-    tar xf $PACKAGE_TAR_TEMP_PATH -C $RUBY_VERSION_PATH
+    tar xf "/tmp/$PACKAGE_TAR_NAME" -C $RUBY_VERSION_PATH
 
     COMPLETE_FILE_PATH="$RUBY_VERSION_PATH/x64.complete"
     if [ ! -f $COMPLETE_FILE_PATH ]; then
