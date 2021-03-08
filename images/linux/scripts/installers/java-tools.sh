7ef43048fbbd04fbe182ac57073bf13404a0df1f
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-03-09 02:00:14 +0300

    move java to toolcache

--- images/linux/scripts/installers/java-tools.sh
+++ images/linux/scripts/installers/java-tools.sh
@@ -7,34 +7,43 @@ source $HELPER_SCRIPTS/install.sh
 source $HELPER_SCRIPTS/os.sh
 source $HELPER_SCRIPTS/etc-environment.sh
 
-JAVA_VERSIONS_LIST=$(get_toolset_value '.java.versions | .[]')
-DEFAULT_JDK_VERSION=$(get_toolset_value '.java.default')
+JAVA_TOOLCACHE_PATH="$AGENT_TOOLSDIRECTORY/Java_Adoptium_jdk"
 
-# Install GPG Key for Adopt Open JDK. See https://adoptopenjdk.net/installation.html
-wget -qO - "https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public" | apt-key add -
-add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+installJavaFromAdoptOpenJDK() {
+    local JAVA_VERSION=$1
 
-if isUbuntu16 || isUbuntu18 ; then
-    # Install GPG Key for Azul Open JDK. See https://www.azul.com/downloads/azure-only/zulu/
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-    apt-add-repository "deb https://repos.azul.com/azure-only/zulu/apt stable main"
-fi
-apt-get update
+    javaRelease=$(curl -s "https://api.adoptopenjdk.net/v3/assets/latest/$JAVA_VERSION/hotspot")
+    archivePath=$(echo $javaRelease | jq -r '[.[] | select(.binary.os=="mac") | .binary.package.link][0]')
+    fullVersion=$(echo $javaRelease | jq -r '[.[] | select(.binary.os=="mac") | .version.openjdk_version][0]')
+    javaToolcacheVersionPath="$JAVA_TOOLCACHE_PATH/$fullVersion"
+    javaToolcacheVersionArchPath="$javaToolcacheVersionPath/x64"
 
-for JAVA_VERSION in ${JAVA_VERSIONS_LIST[@]}; do
-    apt-get -y install adoptopenjdk-$JAVA_VERSION-hotspot=\*
-    echo "JAVA_HOME_${JAVA_VERSION}_X64=/usr/lib/jvm/adoptopenjdk-${JAVA_VERSION}-hotspot-amd64" | tee -a /etc/environment
+    echo "Downloading tar archive $archivePath"
+    download_with_retries $archivePath "/tmp" "OpenJDK$JAVA_VERSION.tar.gz"
+
+    mkdir "/tmp/jdk-$fullVersion" && tar -xzf "/tmp/OpenJDK$JAVA_VERSION.tar.gz" -C "/tmp/jdk-$fullVersion"
+    mkdir -p $javaToolcacheVersionArchPath
+    mv /tmp/jdk-$fullVersion/*/* $javaToolcacheVersionArchPath
+
+    local JAVA_HOME_PATH="$javaToolcacheVersionArchPath/Contents/Home"
+
+    if [[ $JAVA_VERSION == "8" ]]; then
+        echo "export JAVA_HOME=${JAVA_HOME_PATH}" | tee -a /etc/environment
+        export PATH="$JAVA_HOME_PATH:$PATH"
+    else
+        echo "export JAVA_HOME_${JAVA_VERSION}_X64=${JAVA_HOME_PATH}" | tee -a /etc/environment
+    fi
+}
+
+JAVA_VERSIONS_LIST=($(get_toolset_value '.java.versions | .[]'))
+JAVA_DEFAULT=$(get_toolset_value '.java.default')
+
+for JAVA_VERSION in "${JAVA_VERSIONS_LIST[@]}"
+do
+    installJavaFromAdoptOpenJDK $JAVA_VERSION
 done
 
-# Set Default Java version.
-if isUbuntu16; then
-    # issue: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=825987
-    # stackoverflow: https://askubuntu.com/questions/1187136/update-java-alternatives-only-java-but-not-javac-is-changed
-    sed -i 's/(hl|jre|jdk|plugin|DUMMY) /(hl|jre|jdk|jdkhl|plugin|DUMMY) /g' /usr/sbin/update-java-alternatives
-fi
-update-java-alternatives -s /usr/lib/jvm/adoptopenjdk-${DEFAULT_JDK_VERSION}-hotspot-amd64
-
-echo "JAVA_HOME=/usr/lib/jvm/adoptopenjdk-${DEFAULT_JDK_VERSION}-hotspot-amd64" | tee -a /etc/environment
+installJavaFromAdoptOpenJDK $JAVA_DEFAULT
 
 # Install Ant
 apt-fast install -y --no-install-recommends ant ant-optional
@@ -65,4 +74,3 @@ ln -s /usr/share/gradle-"${gradleVersion}"/bin/gradle /usr/bin/gradle
 echo "GRADLE_HOME=/usr/share/gradle" | tee -a /etc/environment
 
 reloadEtcEnvironment
-invoke_tests "Java"
